a6a82f8849e25d4a27add7fb8e9e57716f692450
Deivid Lockwood <deividlock@gmail.com>
2024-01-07 17:41:17 -0500

    adding fibonacci algorithm to php folder

---
+++ php/fibonacci.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Generate Fibonacci Squence
+ * @param int $n the stop
+ * @return array $data the fibonacci sequence array
+ */
+function generateSequnce($n) {
+    $data = [0,1];
+    $i = 2;
+    while($i <= $n) {
+        $data[$i] = $data[$i-1] + $data[$i-2];
+        ++$i;
+    }
+    return $data;
+}
+
+print_r(generateSequnce(10));
